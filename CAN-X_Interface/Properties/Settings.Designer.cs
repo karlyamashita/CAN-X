d6bdb963595b083b9f48c6f26fc3e5349dce272f
Karl Yamashita <344712+karlyamashita@users.noreply.github.com>
2025-09-27 01:40:18 -0700

    Update CAN Jammer Update button to sync icon

--- CAN-X_Interface/Properties/Settings.Designer.cs
+++ CAN-X_Interface/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CAN_X_CAN_Analyzer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -97,7 +97,7 @@ namespace CAN_X_CAN_Analyzer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Version 3.0.2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Version 3.0.4")]
         public string version {
             get {
                 return ((string)(this["version"]));
