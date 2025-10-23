32f11d891ba38b12fdc15e4e4523efd16cb5a90a
Karl Yamashita <344712+karlyamashita@users.noreply.github.com>
2025-10-23 09:16:30 -0700

    Add, Delete, Copy Datagrid. Still need to work on textbox and dropdown

--- CAN-X_Interface/Components/CANable_Devices.xaml.cs
+++ CAN-X_Interface/Components/CANable_Devices.xaml.cs
@@ -29,12 +29,25 @@ namespace CAN_X_CAN_Analyzer.Components
 
         private void DataGridDevices_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-
+            Devices_Data data = dataGridCANableDevices.SelectedItem as Devices_Data; // grabs the current selected row
+            if (data == null) return;
+            TextBoxDeviceDescription.Text = data.Description;
+            TextBoxAPB1Clock.Text = data.APB1_Clock;
         }
 
         private void DataGridDevices_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            DataGridRow dgr = null;
 
+            var visParent = VisualTreeHelper.GetParent(e.OriginalSource as FrameworkElement);
+            while (dgr == null && visParent != null)
+            {
+                dgr = visParent as DataGridRow;
+                visParent = VisualTreeHelper.GetParent(visParent);
+            }
+            if (dgr == null) { return; }
+
+            rowIndexDevices = dgr.GetIndex();
         }
 
         private void ButtonConnect_Click(object sender, RoutedEventArgs e)
@@ -46,6 +59,7 @@ namespace CAN_X_CAN_Analyzer.Components
         {
             var matchFound = true;
             UInt32 newIndex = 0;
+            Devices_Data devices = new Devices_Data();
 
             // check for available key number
             while (matchFound)
@@ -53,13 +67,11 @@ namespace CAN_X_CAN_Analyzer.Components
                 matchFound = false;
                 foreach (var item in dataGridCANableDevices.Items)
                 {
-                    /*
-                    var it = item as CanRxData;
+                    var it = item as Devices_Data;
                     if (it.Key == newIndex)
                     {
                         matchFound = true;
                     }
-                    */
                 }
                 if (matchFound)
                 {
@@ -70,8 +82,8 @@ namespace CAN_X_CAN_Analyzer.Components
                     matchFound = false;
                 }
             }
-            // canRxData.Key = newIndex;
-            // dataGridCANableDevices.Items.Add(canRxData);
+             devices.Key = newIndex;
+             dataGridCANableDevices.Items.Add(devices);
         }
 
         private void ButtonDeleteDevices_Click(object sender, RoutedEventArgs e)
@@ -85,9 +97,59 @@ namespace CAN_X_CAN_Analyzer.Components
 
         private void ButtonCopyDevices_Click(object sender, RoutedEventArgs e)
         {
+            UInt32 newIndex = 0;
 
+            if (dataGridCANableDevices.SelectedItem != null)
+            {
+                Devices_Data selectedItem = (Devices_Data)dataGridCANableDevices.SelectedItem;
+
+                Devices_Data newDeviceData = new Devices_Data
+                {
+                    Description = selectedItem.Description,
+                    COM_Port = selectedItem.COM_Port,
+                    Node = selectedItem.Node,
+                    APB1_Clock = selectedItem.APB1_Clock,
+                    Baud_Rate = selectedItem.Baud_Rate,
+                    CAN_Mode = selectedItem.CAN_Mode,
+                    Connected_Status = selectedItem.Connected_Status
+                };
+
+                foreach (Devices_Data rx in dataGridCANableDevices.Items)
+                {
+                    if (rx.Key > newIndex)
+                    {
+                        newIndex = (UInt32)rx.Key;
+                    }
+                }
+                newDeviceData.Key = newIndex + 1; // update key before adding item
+
+                dataGridCANableDevices.Items.Add(newDeviceData);
+            }
         }
 
+        private void TextBoxDevice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Devices_Data devicesData = (Devices_Data)dataGridCANableDevices.SelectedItem;
 
+            TextBox obj = sender as TextBox;
+            string senderName = obj.Name;
+
+            if (devicesData != null)
+            {
+                switch (senderName)
+                {
+                    case "TextBoxDeviceDescription":
+                        devicesData.Description = TextBoxDeviceDescription.Text;
+                        break;
+                    case "TextBoxAPB1Clock":
+                        devicesData.APB1_Clock = TextBoxAPB1Clock.Text;
+                        break;
+                    default:
+                        break;
+                }
+
+                dataGridCANableDevices.Items.Refresh();
+            }
+        }
     }
 }
---
+++ CAN-X_Interface/Devices_Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAN_X_CAN_Analyzer
+{
+    public class Devices_Data
+    {
+
+        public ulong Key { get; set; } = 0;
+        public string Description { get; set; } = "";
+
+        public string COM_Port { get; set; } = "";
+
+        public string Node { get; set; } = "";
+
+        public string APB1_Clock { get; set; } = "";
+
+        public string Baud_Rate { get; set; } = "";
+
+        public string CAN_Mode { get; set; } = "";
+
+        public string Connected_Status { get; set; } = "";
+
+
+        public Devices_Data()
+        {
+
+        }
+
+
+
+    }
+}
